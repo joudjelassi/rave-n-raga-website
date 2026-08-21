076770f126a7b5ec2f7039addbdbe8261b181dfd
joudjelassi <joudjelassi@gmail.com>
2026-08-21 16:00:45 -0700

    Update team names: Robin Sampayan, Fern Ives

--- src/data/team.js
+++ src/data/team.js
@@ -3,9 +3,9 @@ export const team = [
   { name: 'Joud Jelassi', role: 'Co-Founder' },
   { name: 'Zoe Turcotte (Maldor the Visual)', role: 'Creative Director' },
   { name: 'Jordan Collins (jorb.co)', role: 'Talent Manager / Resident DJ' },
-  { name: 'Robin Michael (psychic.porridge)', role: 'Talent Manager / Resident DJ' },
+  { name: 'Robin Sampayan (psychic.porridge)', role: 'Talent Manager / Resident DJ' },
   { name: 'Micah (micahmakeemtalk)', role: 'Resident DJ' },
   { name: 'Finn Vanderkam (vanderkam)', role: 'Resident DJ' },
   { name: 'Anna-Lyn Albers', role: 'Harm Reduction' },
-  { name: 'Fern', role: 'Harm Reduction' },
+  { name: 'Fern Ives', role: 'Harm Reduction' },
 ];
