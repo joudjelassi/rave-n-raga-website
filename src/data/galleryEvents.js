e09ed01ae366c583a19746d965f2a7be3ebce55b
joudjelassi <joudjelassi@gmail.com>
2026-09-23 23:56:17 -0700

    Add Dropbox link for Rhythm Room Vol 7.0 album

--- src/data/galleryEvents.js
+++ src/data/galleryEvents.js
@@ -1,5 +1,5 @@
 export const galleryEvents = [
-  { title: 'Rhythm Room: Vol. 7.0 — Sands of Sound', dropboxUrl: null },
+  { title: 'Rhythm Room: Vol. 7.0 — Sands of Sound', dropboxUrl: 'https://www.dropbox.com/scl/fo/r6wdew6vgqy6ql5kv4r7t/AKQ0sUARW0MxEzgliLEsuD8?rlkey=1t66wybzjxfwlneajuc0yxnip&st=419z657e&dl=0' },
   { title: 'Rhythm Room: Vol. 6.0 — Estival Solstice', dropboxUrl: 'https://www.dropbox.com/scl/fo/7ltr2c34tsrmids38fc48/ALvx63wd15S19nBt659fnc8?rlkey=e4rh7ju8ux779q5b70eb69k8u&st=xx3sucl1&dl=0' },
   { title: 'Rhythm Room: Vol. 5.0 — Mystic Jungle', dropboxUrl: 'https://www.dropbox.com/scl/fo/fk64roga0jxf1dmkwx63s/ALOvtHJCjZaLxQOboIhPkh4?rlkey=jdwnbhqx6p6g3zgccbkwu577c&st=zfssqt5c&dl=0' },
   { title: 'Rhythm Room: Vol. 4.0 — Cyber Pulse', dropboxUrl: 'https://www.dropbox.com/scl/fo/5e397r83fp3rr6kuz298a/ALCEcbaZHQjtwcdqMI2yjJ8?rlkey=hvtp1rgax50vytcfapqz6yv06&st=mjikkkyp&dl=0' },
